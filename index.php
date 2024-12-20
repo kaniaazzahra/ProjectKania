75cf31f97a69a8df8c9f4a576f3f1da0666aca0c
unknown <kanigitaazzahra9@gmail.com>
2024-12-20 10:46:29 +0700

    second commit add table coloum index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan GIT</title>
+</head>
+<body>
+    <h1>Latihan Git</h1>
+    <tr>
+        <td>Nama</td>
+        <td>Kelas</td>
+</tr>
+    <tr>
+            <td>Kania</td>
+            <td>Kelas XML</td>
+    </tr>
+    <button>Submit</button>
+</body>
+</html>
